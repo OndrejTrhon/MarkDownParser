ec8979b054c273966564d748ec3a8563b5ce5c74
Ondřej Trhoň <ondrej@trhon.cz>
2020-03-28 00:12:59 +0100

    First version yay
    Generous contribution of Petrroll and we're running somehow
    
    Co-Authored-By: Petr Houška <houskape@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using Microsoft.Toolkit.Parsers.Markdown;
+using Microsoft.Toolkit.Parsers.Markdown.Blocks;
+
+namespace MarkDownBlog
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var pathOut = "./output.html";
+            var folderIn = "./";
+
+            using (var fileOut = new StreamWriter(pathOut))
+            {
+                fileOut.Write("PreCSSS");
+
+                var files = Directory.GetFiles(folderIn, "*.txt");
+                ProcessFiles(fileOut, files);
+            }
+        }
+
+        private static void ProcessFiles(StreamWriter fileOut, string[] files)
+        {
+            foreach (var pathIn in files)
+            {
+                using (var fileIn = new StreamReader(pathIn))
+                {
+                    var allText = fileIn.ReadToEnd();
+                    var document = new MarkdownDocument();
+
+                    document.Parse(allText);
+                    foreach (var element in document.Blocks)
+                    {
+                        if (element is HeaderBlock headerBlock)
+                        {
+                            var headerLevel = headerBlock.HeaderLevel;
+                            fileOut.WriteLine($"<h{headerLevel}>{element.ToString()}</h{headerLevel}> ");
+                        }
+                        else if (element is ParagraphBlock)
+                        {
+                            fileOut.WriteLine($"<p>{element.ToString()}</p>");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
