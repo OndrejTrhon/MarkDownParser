fe51e017a511e969ad045e3792b5e35bdf2f0cd6
Ondřej Trhoň <ondrej@trhon.cz>
2020-03-29 00:24:49 +0100

    Creates header div and anchors

--- Program.cs
+++ Program.cs
@@ -15,34 +15,59 @@ namespace MarkDownBlog
 
             using (var fileOut = new StreamWriter(pathOut))
             {
-                fileOut.Write("PreCSSS");
-
                 var files = Directory.GetFiles(folderIn, "*.txt");
+                CreateIndex(fileOut, files);
                 ProcessFiles(fileOut, files);
             }
         }
 
+        private static void CreateIndex(StreamWriter fileOut, string[] files)
+        {
+            fileOut.WriteLine($"<div id=\"index\">");
+            foreach (var pathIn in files)
+            {
+                using var fileIn = new StreamReader(pathIn);
+                int fileCounter = 0;
+                var allText = fileIn.ReadToEnd();
+                var document = new MarkdownDocument();
+
+                document.Parse(allText);
+                foreach (var element in document.Blocks)
+                {
+                    if ((element is HeaderBlock headerBlock) && (headerBlock.HeaderLevel == 1))
+                    {
+                            fileCounter++;
+                            fileCounter.ToString();
+                            fileOut.WriteLine($"<p id=\"headerline\"><a href=\"#{fileCounter}\">header>{element.ToString()}</a></p>");
+                    }
+                }
+
+            }  
+            fileOut.WriteLine($"</div>");
+        }
+        
         private static void ProcessFiles(StreamWriter fileOut, string[] files)
         {
             foreach (var pathIn in files)
             {
-                using (var fileIn = new StreamReader(pathIn))
+                using var fileIn = new StreamReader(pathIn);
+                int fileCounter = 0;
+                var allText = fileIn.ReadToEnd();
+                var document = new MarkdownDocument();
+                document.Parse(allText);
+                
+                foreach (var element in document.Blocks)
                 {
-                    var allText = fileIn.ReadToEnd();
-                    var document = new MarkdownDocument();
-
-                    document.Parse(allText);
-                    foreach (var element in document.Blocks)
+                    if (element is HeaderBlock headerBlock)
                     {
-                        if (element is HeaderBlock headerBlock)
-                        {
-                            var headerLevel = headerBlock.HeaderLevel;
-                            fileOut.WriteLine($"<h{headerLevel}>{element.ToString()}</h{headerLevel}> ");
-                        }
-                        else if (element is ParagraphBlock)
-                        {
+                        var headerLevel = headerBlock.HeaderLevel;
+                        fileCounter++;
+                        fileCounter.ToString();
+                        fileOut.WriteLine($"<h{headerLevel} id=\"{fileCounter}\">{element.ToString()}</h{headerLevel}> ");
+                    }
+                    else if (element is ParagraphBlock)
+                    {
                             fileOut.WriteLine($"<p>{element.ToString()}</p>");
-                        }
                     }
                 }
             }
